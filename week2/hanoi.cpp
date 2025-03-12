4f7b8a7fe6ccbe054a38d9afd2eb51ac2de786e5
nayoungk1m <nadine03@hanyang.ac.kr>
2025-03-12 10:01:15 +0900

    revise warnings

--- week2/hanoi.cpp
+++ week2/hanoi.cpp
@@ -7,11 +7,13 @@ int hanoi(int n, int a=1, int b=2, int c=3) {
     }
     else if (n == 1) {
         std::cout << "Move " << a << " to " << c << std::endl;
+        return 0;
     }
     else {
         hanoi(n-1, a, c, b);
         std::cout << "Move " << a << " to " << c << std::endl;
         hanoi(n-1, b, a, c);
+        return 0;
     }
 }
 
